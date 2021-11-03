2f5c1e9cf0576f3957b35017001cd1fc0a541db6
leehyemin <leeta6824@naver.com>
2021-11-03 13:22:31 +0900

    [21.11.03]quiz.5+6+7+8

--- day04/while_quiz.py
+++ day04/while_quiz.py
@@ -1,11 +1,13 @@
 # 정수 2개 입력가능한 프로그램
+# 5+6+7+8 26 
 
-
-num = int(input("정수1: "))
+num1 = int(input("정수1: "))
 num2 = int(input("정수2: "))
 
-while num + num2:
-    
+total = 0
+while num1 <= num2:
+    total += num1
+    num1 += 1
 
-print(f'정수 두합 :')
+print(f'총합: {total}')
 
